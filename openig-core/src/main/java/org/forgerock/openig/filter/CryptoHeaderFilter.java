ae7f12db1ea1c0379ea43f5c327fcbe846138624
Guillaume Sauthier <guillaume.sauthier@forgerock.com>
2014-05-23 12:07:13 +0000

    OPENIG-115 Increase Unit test coverage for CryptoHeaderFilter
    * Changed protected field to private fields
    * Introduced getters and setters to make it possible to configure them in the unit tests
    * Added missing javadoc

--- openig-core/src/main/java/org/forgerock/openig/filter/CryptoHeaderFilter.java
+++ openig-core/src/main/java/org/forgerock/openig/filter/CryptoHeaderFilter.java
@@ -12,7 +12,7 @@
  * information: "Portions Copyrighted [year] [name of copyright owner]".
  *
  * Copyright © 2010–2011 ApexIdentity Inc. All rights reserved.
- * Portions Copyrighted 2011 ForgeRock AS.
+ * Portions Copyrighted 2011-2014 ForgeRock AS.
  */
 
 package org.forgerock.openig.filter;
@@ -27,6 +27,7 @@ import java.security.InvalidKeyException;
 import java.util.Collections;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Set;
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
@@ -55,28 +56,66 @@ import org.forgerock.openig.util.CaseInsensitiveSet;
  */
 public class CryptoHeaderFilter extends GenericFilter {
 
-    /** TODO: Description. */
+    /**
+     * Default Cipher algorithm to be used when none is specified.
+     */
+    public static final String DEFAULT_ALGORITHM = "DES/ECB/NoPadding";
+
+    /** Should the filter encrypt or decrypt the given headers ? */
     public enum Operation {
-        ENCRYPT, DECRYPT
+        /**
+         * Performs an encryption of the selected headers.
+         */
+        ENCRYPT,
+
+        /**
+         * Perform a decryption of the selected headers.
+         * Notice that the decrypted value is a trimmed String using the given charset ({@code UTF-8} by default).
+         */
+        DECRYPT
     }
 
-    /** TODO: Description. */
-    protected Operation operation;
+    /** Indicated the operation (encryption/decryption) to apply to the headers. */
+    private Operation operation;
 
     /** Indicates the type of message in the exchange to process headers for. */
-    protected MessageType messageType;
+    private MessageType messageType;
 
     /** Cryptographic algorithm. */
-    protected String algorithm;
+    private String algorithm;
 
     /** Encryption key. */
-    protected Key key;
+    private Key key;
 
-    /** TODO: Description. */
-    protected Charset charset;
+    /** Indicates the {@link Charset} to use for decrypted values. */
+    private Charset charset;
 
     /** The names of the headers whose values should be processed for encryption or decryption. */
-    protected final CaseInsensitiveSet headers = new CaseInsensitiveSet();
+    private final Set<String> headers = new CaseInsensitiveSet();
+
+    public void setOperation(final Operation operation) {
+        this.operation = operation;
+    }
+
+    public void setMessageType(final MessageType messageType) {
+        this.messageType = messageType;
+    }
+
+    public void setAlgorithm(final String algorithm) {
+        this.algorithm = algorithm;
+    }
+
+    public void setKey(final Key key) {
+        this.key = key;
+    }
+
+    public void setCharset(final Charset charset) {
+        this.charset = charset;
+    }
+
+    public Set<String> getHeaders() {
+        return headers;
+    }
 
     /**
      * Finds headers marked for processing and encrypts or decrypts the values.
@@ -186,7 +225,7 @@ public class CryptoHeaderFilter extends GenericFilter {
             CryptoHeaderFilter filter = new CryptoHeaderFilter();
             filter.messageType = config.get("messageType").required().asEnum(MessageType.class);
             filter.operation = config.get("operation").required().asEnum(Operation.class);
-            filter.algorithm = config.get("algorithm").defaultTo("DES/ECB/NoPadding").asString();
+            filter.algorithm = config.get("algorithm").defaultTo(DEFAULT_ALGORITHM).asString();
             filter.charset = config.get("charset").defaultTo("UTF-8").asCharset();
             byte[] key = new Base64(0).decode(config.get("key").required().asString());
             if (key.length == 0) {
---
+++ openig-core/src/test/java/org/forgerock/openig/filter/CryptoHeaderFilterTest.java
@@ -0,0 +1,147 @@
+/*
+ * The contents of this file are subject to the terms of the Common Development and
+ * Distribution License (the License). You may not use this file except in compliance with the
+ * License.
+ *
+ * You can obtain a copy of the License at legal/CDDLv1.0.txt. See the License for the
+ * specific language governing permission and limitations under the License.
+ *
+ * When distributing Covered Software, include this CDDL Header Notice in each file and include
+ * the License file at legal/CDDLv1.0.txt. If applicable, add the following below the CDDL
+ * Header, with the fields enclosed by brackets [] replaced by your own identifying
+ * information: "Portions copyright [year] [name of copyright owner]".
+ *
+ * Copyright 2014 ForgeRock AS.
+ */
+
+package org.forgerock.openig.filter;
+
+import org.apache.commons.codec.binary.Base64;
+import org.forgerock.openig.handler.Handler;
+import org.forgerock.openig.http.Exchange;
+import org.forgerock.openig.http.MessageType;
+import org.forgerock.openig.http.Request;
+import org.forgerock.openig.http.Response;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import javax.crypto.spec.SecretKeySpec;
+
+import java.nio.charset.Charset;
+
+import static org.fest.assertions.Assertions.assertThat;
+import static org.mockito.Mockito.doAnswer;
+import static org.testng.Assert.*;
+
+public class CryptoHeaderFilterTest {
+
+    public static final String HEADER_NAME = "Free-Text";
+    public static final String CLEAR_TEXT_VALUE = "Clear text value";
+    public static final String ENCRYPTED_VALUE = "pGuPljcDYhrRRvh8PZ7HXw==";
+    public static final String B64_ENCODED_KEY = "MTIzNDU2Nzg=";
+
+    @Mock
+    private Handler terminalHandler;
+
+    @BeforeMethod
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void testRequestEncryption() throws Exception {
+        CryptoHeaderFilter filter = new CryptoHeaderFilter();
+        filter.setMessageType(MessageType.REQUEST);
+        filter.setOperation(CryptoHeaderFilter.Operation.ENCRYPT);
+        filter.setAlgorithm(CryptoHeaderFilter.DEFAULT_ALGORITHM);
+        filter.getHeaders().add(HEADER_NAME);
+        filter.setKey(buildKey());
+
+        Exchange exchange = new Exchange();
+        exchange.request = new Request();
+
+        // Value length has to be a multiple of 8 because of the DES encryption mechanism
+        exchange.request.headers.putSingle(HEADER_NAME, CLEAR_TEXT_VALUE);
+
+        filter.filter(exchange, terminalHandler);
+
+        assertThat(exchange.request.headers.getFirst(HEADER_NAME))
+                .isEqualTo(ENCRYPTED_VALUE);
+    }
+
+    @Test
+    public void testRequestDecryption() throws Exception {
+        CryptoHeaderFilter filter = new CryptoHeaderFilter();
+        filter.setMessageType(MessageType.REQUEST);
+        filter.setOperation(CryptoHeaderFilter.Operation.DECRYPT);
+        filter.setAlgorithm(CryptoHeaderFilter.DEFAULT_ALGORITHM);
+        filter.getHeaders().add(HEADER_NAME);
+        filter.setKey(buildKey());
+        filter.setCharset(Charset.forName("UTF-8"));
+
+        Exchange exchange = new Exchange();
+        exchange.request = new Request();
+
+        exchange.request.headers.putSingle(HEADER_NAME, ENCRYPTED_VALUE);
+
+        filter.filter(exchange, terminalHandler);
+
+        assertThat(exchange.request.headers.getFirst(HEADER_NAME))
+                .isEqualTo(CLEAR_TEXT_VALUE);
+    }
+
+    @Test
+    public void testResponseEncryption() throws Exception {
+        CryptoHeaderFilter filter = new CryptoHeaderFilter();
+        filter.setMessageType(MessageType.RESPONSE);
+        filter.setOperation(CryptoHeaderFilter.Operation.ENCRYPT);
+        filter.setAlgorithm(CryptoHeaderFilter.DEFAULT_ALGORITHM);
+        filter.getHeaders().add(HEADER_NAME);
+        filter.setKey(buildKey());
+
+        final Exchange exchange = new Exchange();
+        exchange.response = new Response();
+
+        // Value length has to be a multiple of 8 because of the DES encryption mechanism
+        exchange.response.headers.putSingle(HEADER_NAME, CLEAR_TEXT_VALUE);
+
+        filter.filter(exchange, terminalHandler);
+
+        assertThat(exchange.response.headers.getFirst(HEADER_NAME))
+                .isEqualTo(ENCRYPTED_VALUE);
+    }
+
+    @Test
+    public void testResponseDecryption() throws Exception {
+        CryptoHeaderFilter filter = new CryptoHeaderFilter();
+        filter.setMessageType(MessageType.RESPONSE);
+        filter.setOperation(CryptoHeaderFilter.Operation.DECRYPT);
+        filter.setAlgorithm(CryptoHeaderFilter.DEFAULT_ALGORITHM);
+        filter.getHeaders().add(HEADER_NAME);
+        filter.setKey(buildKey());
+        filter.setCharset(Charset.forName("UTF-8"));
+
+        final Exchange exchange = new Exchange();
+        exchange.response = new Response();
+
+        exchange.response.headers.putSingle(HEADER_NAME, ENCRYPTED_VALUE);
+
+        filter.filter(exchange, terminalHandler);
+
+        assertThat(exchange.response.headers.getFirst(HEADER_NAME))
+                .isEqualTo(CLEAR_TEXT_VALUE);
+    }
+
+    /**
+     * DES Key needs to have an 8 bytes length (decoded value)
+     * 12345678 -> MTIzNDU2Nzg=
+     */
+    private static SecretKeySpec buildKey() {
+        return new SecretKeySpec(new Base64(0).decode(B64_ENCODED_KEY), "DES");
+    }
+
+}
