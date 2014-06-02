a53df8abcddad7194e9eac2f367b977b56c0c940
Guillaume Sauthier <guillaume.sauthier@forgerock.com>
2014-06-02 09:43:42 +0000

    OPENIG-115 Covers the regex support

---
+++ openig-core/src/test/java/org/forgerock/openig/regex/StreamPatternMatchesTest.java
@@ -0,0 +1,123 @@
+/*
+ * The contents of this file are subject to the terms of the Common Development and
+ * Distribution License (the License). You may not use this file except in compliance with the
+ * License.
+ *
+ * You can obtain a copy of the License at legal/CDDLv1.0.txt. See the License for the
+ * specific language governing permission and limitations under the License.
+ *
+ * When distributing Covered Software, include this CDDL Header Notice in each file and include
+ * the License file at legal/CDDLv1.0.txt. If applicable, add the following below the CDDL
+ * Header, with the fields enclosed by brackets [] replaced by your own identifying
+ * information: "Portions copyright [year] [name of copyright owner]".
+ *
+ * Copyright 2014 ForgeRock AS.
+ */
+
+package org.forgerock.openig.regex;
+
+import static java.util.Arrays.asList;
+import static org.fest.assertions.Assertions.assertThat;
+import static org.mockito.Mockito.verify;
+
+import java.io.Reader;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+/**
+ * Basically a {@code StreamPatternMatches} is a {@link org.forgerock.openig.regex.StringPatternMatches} that supports
+ * multi-line: it wraps the given reader in a {@link java.io.BufferedReader} and consume it line by line.
+ * For each line, it creates a new {@link org.forgerock.openig.regex.StringPatternMatches} and returns the
+ * produced {@link java.util.regex.Matcher}s.
+ */
+public class StreamPatternMatchesTest {
+
+    @Mock
+    private Reader reader;
+
+    @BeforeMethod
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void testMultiLinePatternNotMatching() throws Exception {
+        Pattern pattern = Pattern.compile("c");
+        StreamPatternMatches matches = new StreamPatternMatches(Readers.reader("aab", "aab"), asList(pattern), true);
+
+        assertThat(matches.hasNext()).isFalse();
+    }
+
+    @Test
+    public void testMultiLinePatternMatching() throws Exception {
+        Pattern pattern = Pattern.compile("a+b");
+        // Needs to set discard to false otherwise, only the first matching result is returned
+        StreamPatternMatches matches = new StreamPatternMatches(Readers.reader("aab", "aab"), asList(pattern), false);
+
+        Matcher matcher = matches.next();
+        assertThat(matcher.group()).isEqualTo("aab");
+        matcher = matches.next();
+        assertThat(matcher.group()).isEqualTo("ab");
+
+        matcher = matches.next();
+        assertThat(matcher.group()).isEqualTo("aab");
+        matcher = matches.next();
+        assertThat(matcher.group()).isEqualTo("ab");
+
+        assertThat(matches.hasNext()).isFalse();
+    }
+
+    @Test
+    public void testCLoseIsPropagated() throws Exception {
+        Pattern pattern = Pattern.compile(".*");
+        StreamPatternMatches matches = new StreamPatternMatches(reader, asList(pattern), true);
+
+        matches.close();
+
+        verify(reader).close();
+    }
+
+    @Test
+    public void testMultiLineMultiPatternMatching() throws Exception {
+
+        StreamPatternMatches matches = new StreamPatternMatches(
+                Readers.reader("aab", "aabc"),
+                asList(
+                        Pattern.compile("ab"),
+                        Pattern.compile("bc")),
+                false);
+
+        Matcher matcher = matches.next();
+        assertThat(matcher.group()).isEqualTo("ab");
+        matcher = matches.next();
+        assertThat(matcher.group()).isEqualTo("ab");
+        matcher = matches.next();
+        assertThat(matcher.group()).isEqualTo("bc");
+
+        assertThat(matches.hasNext()).isFalse();
+    }
+
+    @Test
+    public void testMultiLineMultiPatternMatchingAndDiscrad() throws Exception {
+
+        StreamPatternMatches matches = new StreamPatternMatches(
+                Readers.reader("aab", "aabc"),
+                asList(
+                        Pattern.compile("ab"),
+                        Pattern.compile("bc")),
+                true);
+
+        Matcher matcher = matches.next();
+        assertThat(matcher.group()).isEqualTo("ab");
+        matcher = matches.next();
+        assertThat(matcher.group()).isEqualTo("bc");
+
+        assertThat(matches.hasNext()).isFalse();
+    }
+
+}
---
+++ openig-core/src/test/java/org/forgerock/openig/regex/StringPatternMatchesTest.java
@@ -0,0 +1,73 @@
+/*
+ * The contents of this file are subject to the terms of the Common Development and
+ * Distribution License (the License). You may not use this file except in compliance with the
+ * License.
+ *
+ * You can obtain a copy of the License at legal/CDDLv1.0.txt. See the License for the
+ * specific language governing permission and limitations under the License.
+ *
+ * When distributing Covered Software, include this CDDL Header Notice in each file and include
+ * the License file at legal/CDDLv1.0.txt. If applicable, add the following below the CDDL
+ * Header, with the fields enclosed by brackets [] replaced by your own identifying
+ * information: "Portions copyright [year] [name of copyright owner]".
+ *
+ * Copyright 2014 ForgeRock AS.
+ */
+
+package org.forgerock.openig.regex;
+
+import static java.util.Arrays.asList;
+import static org.fest.assertions.Assertions.assertThat;
+
+import java.util.NoSuchElementException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.testng.annotations.Test;
+
+public class StringPatternMatchesTest {
+
+    @Test
+    public void testWithNoMatchingMatchers() throws Exception {
+        Pattern pattern = Pattern.compile("c");
+        StringPatternMatches matches = new StringPatternMatches("aaab", asList(pattern), true);
+
+        assertThat(matches.hasNext()).isFalse();
+    }
+
+    @Test(expectedExceptions = NoSuchElementException.class)
+    public void testWithNoMatchingMatchersThrowException() throws Exception {
+        Pattern pattern = Pattern.compile("c");
+        StringPatternMatches matches = new StringPatternMatches("aaab", asList(pattern), true);
+
+        matches.next();
+    }
+
+    @Test
+    public void testMatchingMatchersAreDiscarded() throws Exception {
+        Pattern pattern = Pattern.compile("a*b");
+        StringPatternMatches matches = new StringPatternMatches("aaab", asList(pattern), true);
+
+        Matcher matcher = matches.next();
+        assertThat(matcher.matches()).isTrue();
+        assertThat(matcher.group()).isEqualTo("aaab");
+        assertThat(matches.hasNext()).isFalse();
+    }
+
+    @Test
+    public void testMatchingMatchersAreNotDiscarded() throws Exception {
+        Pattern pattern = Pattern.compile("a+b");
+        StringPatternMatches matches = new StringPatternMatches("aaab", asList(pattern), false);
+
+        Matcher matcher = matches.next();
+        assertThat(matcher.group()).isEqualTo("aaab");
+
+        matcher = matches.next();
+        assertThat(matcher.group()).isEqualTo("aab");
+
+        matcher = matches.next();
+        assertThat(matcher.group()).isEqualTo("ab");
+
+        assertThat(matches.hasNext()).isFalse();
+    }
+}
