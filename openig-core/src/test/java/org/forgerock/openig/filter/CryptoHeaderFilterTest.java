e52fe0921fd79ff83e556f6b8d87d1ddf84d9bc7
Violette Roche-Montane <violette.roche-montane@forgerock.com>
2014-11-13 15:27:58 +0000

    CR-5263 OpenIG-128 Change default cipher algorithm used in CryptoHeaderFilter - Changed the default cipher algorithm from "DES/ECB/NoPadding" to "AES/ECB/PKCS5Padding". - Modified/added unit tests according to the new selected default cipher. - Updated javadoc.

--- openig-core/src/test/java/org/forgerock/openig/filter/CryptoHeaderFilterTest.java
+++ openig-core/src/test/java/org/forgerock/openig/filter/CryptoHeaderFilterTest.java
@@ -16,12 +16,24 @@
 
 package org.forgerock.openig.filter;
 
+import static java.nio.charset.Charset.*;
+import static java.security.KeyPairGenerator.*;
 import static org.assertj.core.api.Assertions.*;
 import static org.forgerock.json.fluent.JsonValue.*;
+import static org.forgerock.openig.filter.CryptoHeaderFilter.*;
+import static org.forgerock.openig.filter.CryptoHeaderFilter.Operation.*;
+import static org.forgerock.openig.http.MessageType.*;
 import static org.forgerock.openig.log.LogSink.*;
+import static org.forgerock.util.encode.Base64.*;
+import static org.mockito.Matchers.*;
+import static org.mockito.Mockito.*;
 
-import java.nio.charset.Charset;
+import java.security.Key;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
 
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.DESedeKeySpec;
 import javax.crypto.spec.SecretKeySpec;
 
 import org.forgerock.json.fluent.JsonValue;
@@ -29,13 +41,16 @@ import org.forgerock.json.fluent.JsonValueException;
 import org.forgerock.openig.handler.Handler;
 import org.forgerock.openig.heap.Name;
 import org.forgerock.openig.http.Exchange;
-import org.forgerock.openig.http.MessageType;
 import org.forgerock.openig.http.Request;
 import org.forgerock.openig.http.Response;
-import org.forgerock.util.encode.Base64;
+import org.forgerock.openig.log.LogLevel;
+import org.forgerock.openig.log.Logger;
+import org.forgerock.openig.log.NullLogSink;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 @SuppressWarnings("javadoc")
@@ -43,11 +58,36 @@ public class CryptoHeaderFilterTest {
 
     public static final String HEADER_NAME = "Free-Text";
     public static final String CLEAR_TEXT_VALUE = "Clear text value";
-    public static final String ENCRYPTED_VALUE = "pGuPljcDYhrRRvh8PZ7HXw==";
-    public static final String B64_ENCODED_KEY = "MTIzNDU2Nzg=";
+    public static final String ENCRYPTED_VALUE = "uPIQxqe5mbcnMw/SlMl0tvRRYGjkU77b/7tz3twFxRc=";
+    /** The secret key length should be 16 with AES */
+    public static final String B64_ENCODED_KEY = "VGhpc0lzQVNlY3JldEtleQ==";
+
+    /** Cipher algorithms required in a standard JVM. */
+    @DataProvider
+    private Object[][] algorithms() {
+        return new Object[][] {
+            { "AES/CBC/NoPadding" },
+            { "AES/CBC/PKCS5Padding" },
+            { "AES/ECB/NoPadding" },
+            { "AES/ECB/PKCS5Padding" },
+            { "DES/CBC/NoPadding" },
+            { "DES/CBC/PKCS5Padding" },
+            { "DES/ECB/NoPadding" },
+            { "DES/ECB/PKCS5Padding" },
+            { "DESede/CBC/NoPadding" },
+            { "DESede/CBC/PKCS5Padding" },
+            { "DESede/ECB/NoPadding" },
+            { "DESede/ECB/PKCS5Padding" },
+            { "RSA/ECB/PKCS1Padding" },
+            { "RSA/ECB/OAEPWithSHA-1AndMGF1Padding" },
+            { "RSA/ECB/OAEPWithSHA-256AndMGF1Padding" } };
+    }
 
     @Mock
     private Handler terminalHandler;
+
+    @Spy
+    private Logger logger = new Logger(new NullLogSink(), Name.of("source"));
 
     @BeforeMethod
     public void setUp() throws Exception {
@@ -55,35 +95,64 @@ public class CryptoHeaderFilterTest {
     }
 
     @Test
-    public void testRequestEncryption() throws Exception {
-        CryptoHeaderFilter filter = new CryptoHeaderFilter();
-        filter.setMessageType(MessageType.REQUEST);
-        filter.setOperation(CryptoHeaderFilter.Operation.ENCRYPT);
-        filter.setAlgorithm(CryptoHeaderFilter.DEFAULT_ALGORITHM);
-        filter.getHeaders().add(HEADER_NAME);
-        filter.setKey(buildKey());
+    public void shouldEncryptionUsingDefaultAlgorithmWithInvalidKeyReturnsNull() throws Exception {
+        CryptoHeaderFilter filter = buildDefaultCryptoHeader();
+        filter.setMessageType(REQUEST);
+        filter.setKey(new SecretKeySpec(decode("zuul"), "AES"));
 
         Exchange exchange = new Exchange();
         exchange.request = new Request();
 
-        // Value length has to be a multiple of 8 because of the DES encryption mechanism
         exchange.request.getHeaders().putSingle(HEADER_NAME, CLEAR_TEXT_VALUE);
 
         filter.filter(exchange, terminalHandler);
+
+        verify(logger).logMessage(any(LogLevel.class), anyString());
+
+        assertThat(exchange.request.getHeaders().getFirst(HEADER_NAME)).isNull();
+    }
+
+    @Test
+    public void shouldEncryptionWithInvalidAlgorithmReturnsNull() throws Exception {
+        CryptoHeaderFilter filter = buildDefaultCryptoHeader();
+        filter.setMessageType(REQUEST);
+        filter.setAlgorithm("Unknown");
+
+        Exchange exchange = new Exchange();
+        exchange.request = new Request();
+
+        exchange.request.getHeaders().putSingle(HEADER_NAME, CLEAR_TEXT_VALUE);
+
+        filter.filter(exchange, terminalHandler);
+
+        verify(logger).logMessage(any(LogLevel.class), anyString());
+
+        assertThat(exchange.request.getHeaders().getFirst(HEADER_NAME)).isNull();
+    }
+
+    @Test
+    public void testRequestEncryptionUsingDefaultAlgorithm() throws Exception {
+        CryptoHeaderFilter filter = buildDefaultCryptoHeader();
+        filter.setMessageType(REQUEST);
+
+        Exchange exchange = new Exchange();
+        exchange.request = new Request();
+
+        exchange.request.getHeaders().putSingle(HEADER_NAME, CLEAR_TEXT_VALUE);
+
+        filter.filter(exchange, terminalHandler);
+
+        verifyZeroInteractions(logger);
 
         assertThat(exchange.request.getHeaders().getFirst(HEADER_NAME))
                 .isEqualTo(ENCRYPTED_VALUE);
     }
 
     @Test
-    public void testRequestDecryption() throws Exception {
-        CryptoHeaderFilter filter = new CryptoHeaderFilter();
-        filter.setMessageType(MessageType.REQUEST);
-        filter.setOperation(CryptoHeaderFilter.Operation.DECRYPT);
-        filter.setAlgorithm(CryptoHeaderFilter.DEFAULT_ALGORITHM);
-        filter.getHeaders().add(HEADER_NAME);
-        filter.setKey(buildKey());
-        filter.setCharset(Charset.forName("UTF-8"));
+    public void testRequestDecryptionUsingDefaultAlgorithm() throws Exception {
+        CryptoHeaderFilter filter = buildDefaultCryptoHeader();
+        filter.setMessageType(REQUEST);
+        filter.setOperation(DECRYPT);
 
         Exchange exchange = new Exchange();
         exchange.request = new Request();
@@ -92,40 +161,35 @@ public class CryptoHeaderFilterTest {
 
         filter.filter(exchange, terminalHandler);
 
+        verifyZeroInteractions(logger);
+
         assertThat(exchange.request.getHeaders().getFirst(HEADER_NAME))
                 .isEqualTo(CLEAR_TEXT_VALUE);
     }
 
     @Test
-    public void testResponseEncryption() throws Exception {
-        CryptoHeaderFilter filter = new CryptoHeaderFilter();
-        filter.setMessageType(MessageType.RESPONSE);
-        filter.setOperation(CryptoHeaderFilter.Operation.ENCRYPT);
-        filter.setAlgorithm(CryptoHeaderFilter.DEFAULT_ALGORITHM);
-        filter.getHeaders().add(HEADER_NAME);
-        filter.setKey(buildKey());
+    public void testResponseEncryptionUsingDefaultAlgorithm() throws Exception {
+        CryptoHeaderFilter filter = buildDefaultCryptoHeader();
+        filter.setMessageType(RESPONSE);
 
         final Exchange exchange = new Exchange();
         exchange.response = new Response();
 
-        // Value length has to be a multiple of 8 because of the DES encryption mechanism
         exchange.response.getHeaders().putSingle(HEADER_NAME, CLEAR_TEXT_VALUE);
 
         filter.filter(exchange, terminalHandler);
+
+        verifyZeroInteractions(logger);
 
         assertThat(exchange.response.getHeaders().getFirst(HEADER_NAME))
                 .isEqualTo(ENCRYPTED_VALUE);
     }
 
     @Test
-    public void testResponseDecryption() throws Exception {
-        CryptoHeaderFilter filter = new CryptoHeaderFilter();
-        filter.setMessageType(MessageType.RESPONSE);
+    public void testResponseDecryptionUsingDefaultAlgorithm() throws Exception {
+        CryptoHeaderFilter filter = buildDefaultCryptoHeader();
+        filter.setMessageType(RESPONSE);
         filter.setOperation(CryptoHeaderFilter.Operation.DECRYPT);
-        filter.setAlgorithm(CryptoHeaderFilter.DEFAULT_ALGORITHM);
-        filter.getHeaders().add(HEADER_NAME);
-        filter.setKey(buildKey());
-        filter.setCharset(Charset.forName("UTF-8"));
 
         final Exchange exchange = new Exchange();
         exchange.response = new Response();
@@ -134,8 +198,36 @@ public class CryptoHeaderFilterTest {
 
         filter.filter(exchange, terminalHandler);
 
+        verifyZeroInteractions(logger);
+
         assertThat(exchange.response.getHeaders().getFirst(HEADER_NAME))
                 .isEqualTo(CLEAR_TEXT_VALUE);
+    }
+
+    @Test(dataProvider = "algorithms")
+    public void testEncryptionAlgorithms(final String algorithm) throws Exception {
+        CryptoHeaderFilter filter = new CryptoHeaderFilter();
+        filter.setMessageType(REQUEST);
+        filter.setOperation(ENCRYPT);
+        filter.setAlgorithm(algorithm);
+
+        filter.setKey(buildKey(algorithm));
+        filter.getHeaders().add(HEADER_NAME);
+        filter.logger = logger;
+
+        Exchange exchange = new Exchange();
+        exchange.request = new Request();
+
+        exchange.request.getHeaders().putSingle(HEADER_NAME, CLEAR_TEXT_VALUE);
+
+        filter.filter(exchange, terminalHandler);
+
+        verifyZeroInteractions(logger);
+
+        assertThat(exchange.request.getHeaders().getFirst(HEADER_NAME))
+                .isNotNull()
+                .isNotEmpty()
+                .isNotEqualTo(CLEAR_TEXT_VALUE);
     }
 
     @Test(expectedExceptions = JsonValueException.class)
@@ -143,18 +235,51 @@ public class CryptoHeaderFilterTest {
         CryptoHeaderFilter.Heaplet heaplet = new CryptoHeaderFilter.Heaplet();
         JsonValue config = json(object(field("messageType", "REQUEST"),
                                        field("operation", "DECRYPT"),
-                                       field("key", "DESKEY"))); // Not a valid DES Key format
+                                       field("key", "DESKEY"))); // Not a valid key format
 
         // Note: I've used the special name LogSink to avoid having to configure a real heap
         heaplet.create(Name.of(LOGSINK_HEAP_KEY), config, null);
     }
 
-    /**
-     * DES Key needs to have an 8 bytes length (decoded value)
-     * 12345678 -> MTIzNDU2Nzg=
-     */
-    private static SecretKeySpec buildKey() {
-        return new SecretKeySpec(Base64.decode(B64_ENCODED_KEY), "DES");
+    private CryptoHeaderFilter buildDefaultCryptoHeader() {
+        final CryptoHeaderFilter filter = new CryptoHeaderFilter();
+        filter.setOperation(ENCRYPT);
+        filter.setAlgorithm(DEFAULT_ALGORITHM);
+        filter.getHeaders().add(HEADER_NAME);
+        filter.setKey(buildKey(DEFAULT_ALGORITHM));
+        filter.setCharset(forName("UTF-8"));
+        filter.logger = logger;
+        return filter;
     }
 
+    /** Generates a key according to the algorithm in use. */
+    private static Key buildKey(final String algorithm) {
+        if (algorithm.contains("DESede")) {
+            SecretKeyFactory factory;
+            try {
+                factory = SecretKeyFactory.getInstance("DESede");
+                // Key length should be at least 24 bytes.
+                return factory.generateSecret(new DESedeKeySpec(
+                        decode("U2VjcmV0S2V5Q29udGFpbmluZzI0Q2hh")));
+            } catch (Exception e) {
+                return null;
+            }
+        } else if (algorithm.contains("DES")) {
+            // Key length should be 8 bytes.
+            return new SecretKeySpec(decode("MTIzNDU2Nzg="), "DES");
+        } else if (algorithm.contains("RSA")) {
+            KeyPairGenerator kpGenerator;
+            try {
+                kpGenerator = getInstance("RSA");
+            } catch (NoSuchAlgorithmException e) {
+                return null;
+            }
+            // 1024, 2048 supported.
+            kpGenerator.initialize(1024);
+            return kpGenerator.genKeyPair().getPublic();
+        } else {
+            // Key length should be 16 bytes.
+            return new SecretKeySpec(decode(B64_ENCODED_KEY), "AES");
+        }
+    }
 }
