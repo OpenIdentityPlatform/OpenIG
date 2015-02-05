0339032223848e88d0344cb0921175f8f2c09794
Guillaume Sauthier <guillaume.sauthier@forgerock.com>
2015-02-05 09:43:26 +0000

    Fix gremlins character (Unicode EN DASH) in old copyright ranges

--- openig-core/src/main/java/org/forgerock/openig/util/StringUtil.java
+++ openig-core/src/main/java/org/forgerock/openig/util/StringUtil.java
@@ -12,7 +12,7 @@
  * information: "Portions Copyright [year] [name of copyright owner]".
  *
  * Copyright 2009 Sun Microsystems Inc.
- * Portions Copyright 2010–2011 ApexIdentity Inc.
+ * Portions Copyright 2010-2011 ApexIdentity Inc.
  * Portions Copyright 2011-2014 ForgeRock AS.
  */
 
