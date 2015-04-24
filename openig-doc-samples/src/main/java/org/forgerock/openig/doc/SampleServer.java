55e7c2d8f8cce4e6e621a588f0e4faf2b8a0e962
Mark Craig <mark.craig@forgerock.com>
2015-04-24 12:55:06 +0000

    CR-6746 OPENIG-490 Allow simultaneous use of all example routes
    This patch updates the examples in the documentation
    so that they are all routes and can be used at the same time.
    
    Some examples still require additional configuration.
    
    This patch also updates the sample doc server
    for use with the revised examples.
    
    This patch also adds support for authentication with headers,
    as shown in the following example:
    
    $ curl \
     --request POST \
     --header "username: bjensen" \
     --header "password: hifalutin" \
     http://localhost:8081/
    
    This capability is not yet used in the docs,
    but in review we found it could be useful for QA.

--- openig-doc-samples/src/main/java/org/forgerock/openig/doc/SampleServer.java
+++ openig-doc-samples/src/main/java/org/forgerock/openig/doc/SampleServer.java
@@ -11,18 +11,22 @@
  * Header, with the fields enclosed by brackets [] replaced by your own identifying
  * information: "Portions Copyright [year] [name of copyright owner]".
  *
- * Copyright 2014 ForgeRock AS.
+ * Copyright 2014-2015 ForgeRock AS.
  */
 
 package org.forgerock.openig.doc;
 
+import org.glassfish.grizzly.http.Cookie;
 import org.glassfish.grizzly.http.Method;
 import org.glassfish.grizzly.http.server.HttpHandler;
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.grizzly.http.server.NetworkListener;
 import org.glassfish.grizzly.http.server.Request;
 import org.glassfish.grizzly.http.server.Response;
+import org.glassfish.grizzly.ssl.SSLContextConfigurator;
+import org.glassfish.grizzly.ssl.SSLEngineConfigurator;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
@@ -38,27 +42,34 @@ public final class SampleServer {
 
     private static final String EOL = System.getProperty("line.separator");
     private static final Logger LOGGER = Logger.getLogger(SampleServer.class.getName());
+    private static final int DEFAULT_PORT = 8081;
+    private static final int DEFAULT_SSL_PORT = 8444;
 
     /**
      * Start an HTTP server.
      *
-     * @param args Optionally specify a free port number. Default: 8081.
+     * @param args Optionally specify a free port number and free SSL port number.
+     *             Defaults: 8081 (HTTP), 8444 (HTTPS).
      */
     public static void main(String[] args) {
-        final String usage = "Specify an optional port number. Default: 8081.";
-        int port = 8081;
+        final String usage = "Optionally specify HTTP and HTTPS port numbers. Defaults: 8081, 8444.";
+        int port = DEFAULT_PORT;
+        int sslPort = DEFAULT_SSL_PORT;
 
-        if (args.length > 1) {
+        if (args.length > 2) {
             System.out.println(usage);
             System.exit(-1);
         }
 
-        if (args.length == 1) {
+        if (args.length > 0) {
             port = Integer.parseInt(args[0]);
+            if (args.length == 2) {
+                sslPort = Integer.parseInt(args[1]);
+            }
         }
 
         LOGGER.setLevel(Level.INFO);
-        runServer(port);
+        runServer(port, sslPort);
     }
 
     /**
@@ -68,10 +79,11 @@ public final class SampleServer {
      * <p>
      * On HTTP PUT with valid credentials, the server returns a profile page.
      *
-     * @param port Port on which the server listens
+     * @param port      Port on which the server listens for HTTP
+     * @param sslPort   Port on which the server listens for HTTPS
      */
-    static void runServer(int port) {
-        start(port, true);
+    static void runServer(int port, int sslPort) {
+        start(port, sslPort, true);
     }
 
     /**
@@ -79,26 +91,35 @@ public final class SampleServer {
      * <p>
      * Use stop() to shut the server down.
      *
-     * @param port Port on which the server listens
+     * @param port Port on which the server listens for HTTP
      * @return The HttpServer that is running if letRun is true
      */
-    static HttpServer start(final int port) {
-        return start(port, false);
+    static HttpServer start(final int port, final int sslPort) {
+        return start(port, sslPort, false);
     }
 
     /**
      * Run the HTTP server, listening on the chosen port.
      *
-     * @param port Port on which the server listens
-     * @param waitForCtrlC If true, only stop the server when the user enters Ctrl+C
+     * @param port          Port on which the server listens for HTTP
+     * @param sslPort       Port on which the server listens for HTTPS
+     * @param waitForCtrlC  If true, only stop the server when the user enters Ctrl+C
      * @return The HttpServer that is running if letRun is true
      */
-    static HttpServer start(final int port, final boolean waitForCtrlC) {
+    static HttpServer start(final int port, final int sslPort, final boolean waitForCtrlC) {
 
         final HttpServer httpServer = new HttpServer();
-        final NetworkListener networkListener =
-                new NetworkListener("sample-server", "0.0.0.0", port);
-        httpServer.addListener(networkListener);
+        System.out.println("Preparing to listen for HTTP on port " + port + ".");
+        httpServer.addListener(new NetworkListener("HTTP", "0.0.0.0", port));
+        SSLEngineConfigurator sslEngineConfigurator = createSslConfiguration();
+        if (sslEngineConfigurator != null) {
+            System.out.println("Preparing to listen for HTTPS on port " + sslPort + ".");
+            System.out.println("The server will use a self-signed certificate not known to browsers.");
+            System.out.println("When using HTTPS with curl for example, try --insecure.");
+            httpServer.addListener(new NetworkListener("HTTPS", "0.0.0.0", sslPort));
+            httpServer.getListener("HTTPS").setSSLEngineConfig(sslEngineConfigurator);
+            httpServer.getListener("HTTPS").setSecure(true);
+        }
         httpServer.getServerConfiguration().addHttpHandler(new SampleHandler());
 
         if (waitForCtrlC) {
@@ -111,10 +132,10 @@ public final class SampleServer {
         }
 
         try {
-            LOGGER.info("Starting HTTP server on port " + port);
+            System.out.println("Starting server...");
             httpServer.start();
             if (waitForCtrlC) {
-                LOGGER.info("Press Ctrl+C to stop the server.");
+                System.out.println("Press Ctrl+C to stop the server.");
                 Thread.currentThread().join();
             }
         } catch (Exception e) {
@@ -134,12 +155,89 @@ public final class SampleServer {
     }
 
     /**
+     * Returns an SSL configuration that uses {@code keystore.jks}.
+     *
+     * <br>
+     *
+     * The key store has one key pair with a self-signed cert, {@code test}.
+     * The key store was set up with the following commands:
+     *
+     * <pre>
+     * keytool \
+     *  -genkey \
+     *  -alias test \
+     *  -keyalg rsa \
+     *  -dname "cn=Doc Sample Server,dc=openig,dc=example,dc=com" \
+     *  -keystore keystore.jks \
+     *  -storepass changeit \
+     *  -keypass changeit
+     *
+     * keytool \
+     *  -selfcert \
+     *  -alias test \
+     *  -validity 7300 \
+     *  -keystore keystore.jks \
+     *  -storepass changeit \
+     *  -keypass changeit
+     * </pre>
+     *
+     * @return An SSL configuration that uses {@code keystore.jks},
+     *         or null if no valid SSL configuration could be set up.
+     */
+    private static SSLEngineConfigurator createSslConfiguration() {
+        SSLContextConfigurator sslContextConfigurator = new SSLContextConfigurator();
+        try {
+            // Cannot read the key store as a file inside a jar,
+            // so get the key store as a byte array.
+            sslContextConfigurator.setKeyStoreBytes(getKeyStore());
+            sslContextConfigurator.setKeyStorePass("changeit");
+            sslContextConfigurator.setKeyPass("changeit");
+        } catch (IOException e) {
+            LOGGER.info("Failed to load key store when setting up HTTPS.");
+            e.printStackTrace();
+            return null;
+        }
+
+        if (sslContextConfigurator.validateConfiguration(true)) {
+            SSLEngineConfigurator sslEngineConfigurator =
+                    new SSLEngineConfigurator(sslContextConfigurator.createSSLContext());
+            sslEngineConfigurator.setClientMode(false);
+            sslEngineConfigurator.setNeedClientAuth(false);
+            sslEngineConfigurator.setWantClientAuth(false);
+            return sslEngineConfigurator;
+        } else {
+            LOGGER.info("Failed to build a valid HTTPS configuration.");
+            return null;
+        }
+    }
+
+    /**
+     * Returns {@code keystore.jks} as a byte array.
+     * @return {@code keystore.jks} as a byte array.
+     * @throws IOException  Failed to read {@code keystore.jks}.
+     */
+    private static byte[] getKeyStore() throws IOException {
+        InputStream inputStream = SampleServer.class.getResourceAsStream("/keystore.jks");
+        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+        int read;
+        byte[] data = new byte[4096];
+        while ((read = inputStream.read(data, 0, data.length)) != -1) {
+            buffer.write(data, 0, read);
+        }
+        return buffer.toByteArray();
+    }
+
+    /**
      * Handler for HTTP GET and HTTP PUT requests.
      */
     static class SampleHandler extends HttpHandler {
 
         @Override
         public void service(Request request, Response response) throws Exception {
+            if (request.getHttpHandlerPath().equalsIgnoreCase("/login")) {
+                response.addCookie(new Cookie("login-cookie", "chocolate chip"));
+            }
+
             if (Method.GET == request.getMethod()) {
                 String homePage = getResourceAsString("/home.html");
 
@@ -150,9 +248,34 @@ public final class SampleServer {
             }
 
             if (Method.POST == request.getMethod()) {
+                String username;
+                String password;
 
-                final String username = request.getParameter("username");
-                final String password = request.getParameter("password");
+                // Allow use of IDToken1 (username) and IDToken2 (password)
+                // to simulate the behavior of the OpenAM classic UI login page.
+                username = request.getParameter("IDToken1");
+                password = request.getParameter("IDToken2");
+                if (username != null && password != null) {
+                    simulateOpenAMResponse(username, password, response);
+                    return;
+                }
+
+                // Accept username and password as headers for testing.
+                if (notNullOrEmpty(request.getHeader("username"))) {
+                    username = request.getHeader("username");
+                }
+                if (notNullOrEmpty(request.getHeader("password"))) {
+                    password = request.getHeader("password");
+                }
+
+                // Accept username and password as parameters
+                // in the query string or as form-encoded data.
+                if (notNullOrEmpty(request.getParameter("username"))) {
+                    username = request.getParameter("username");
+                }
+                if (notNullOrEmpty(request.getParameter("password"))) {
+                    password = request.getParameter("password");
+                }
 
                 if (username == null || password == null) {
                     final String authRequired = "Authorization Required";
@@ -196,6 +319,48 @@ public final class SampleServer {
                 }
             }
         }
+
+        /**
+         * Returns true if the String to test is neither null nor empty.
+         * @param s The String to test.
+         * @return true if the String to test is neither null nor empty.
+         */
+        private boolean notNullOrEmpty(final String s) {
+            return s != null && !s.isEmpty();
+        }
+    }
+
+    /**
+     * Simulates a response from OpenAM.
+     *
+     * <br>
+     *
+     * If the username and password are valid, sets response status to 200 OK
+     * and writes a text message and fake SSO Token cookie in the response.
+     *
+     * Otherwise, sets reponse status to 403 Forbidden
+     * and writes a failure text message in the response.
+     *
+     * @param username      A username such as {@code demo}
+     * @param password      A password such as {@code changeit}
+     * @param response      The response to the request
+     * @throws IOException  Failed when checking credentials
+     */
+    private static void simulateOpenAMResponse(final String username,
+                                               final String password,
+                                               final Response response) throws IOException {
+        String message;
+        if (credentialsAreValid(username, password)) {
+            message = "Welcome, " + username + "!" + EOL;
+            response.addCookie(new Cookie("iPlanetDirectoryPro", "fakeSsoToken"));
+            response.setStatus(200, "OK");
+        } else {
+            message = "Too bad, " + username + ", you failed to authenticate." + EOL;
+            response.setStatus(403, "Forbidden");
+        }
+        response.setContentType("text/plain");
+        response.setContentLength(message.length());
+        response.getWriter().write(message);
     }
 
     /**
