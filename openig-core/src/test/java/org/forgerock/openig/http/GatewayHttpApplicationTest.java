be5dca733eff2f9fe69f642cc04d05bf50a43ed3
Laurent Vaills <laurent.vaills@forgerock.com>
2015-07-03 17:04:44 +0000

    OPENIG-562 CR-7446 Reserve the route /openig

--- openig-core/src/test/java/org/forgerock/openig/http/GatewayHttpApplicationTest.java
+++ openig-core/src/test/java/org/forgerock/openig/http/GatewayHttpApplicationTest.java
@@ -18,12 +18,24 @@ package org.forgerock.openig.http;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.failBecauseExceptionWasNotThrown;
+import java.io.IOException;
+import java.util.HashMap;
 
+import org.forgerock.http.ClientInfoContext;
+import org.forgerock.http.Handler;
 import org.forgerock.http.HttpApplicationException;
+import org.forgerock.http.HttpContext;
+import org.forgerock.http.RootContext;
+import org.forgerock.http.Session;
+import org.forgerock.http.protocol.Request;
+import org.forgerock.http.protocol.Response;
+import org.forgerock.http.protocol.Status;
 import org.forgerock.openig.config.Environment;
 import org.forgerock.openig.config.env.DefaultEnvironment;
 import org.forgerock.openig.handler.router.DestroyDetectHandler;
 import org.forgerock.openig.handler.router.Files;
+import org.forgerock.util.promise.NeverThrowsException;
+import org.forgerock.util.promise.Promise;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
@@ -50,5 +62,40 @@ public class GatewayHttpApplicationTest {
         } catch (HttpApplicationException e) {
             assertThat(DestroyDetectHandler.destroyed).isTrue();
         }
+    }
+
+    @Test
+    public void shouldReserveOpenIGPath() throws Exception {
+        Environment env = new DefaultEnvironment(Files.getRelative(getClass(), "teapot"));
+        GatewayHttpApplication application = new GatewayHttpApplication(env);
+        Handler start = application.start();
+
+        Promise<Response,NeverThrowsException> promise;
+        Request request;
+
+        // This request must not be handled by the root handler
+        request = new Request();
+        request.setUri("/openig");
+        promise = start.handle(new RootContext(), request);
+        assertThat(promise.get().getStatus()).isNotEqualTo(Status.TEAPOT);
+
+        // This request must be handled by the root handler
+        request = new Request();
+        request.setUri("/foo");
+        promise = start.handle(buildContext(), request);
+        assertThat(promise.get().getStatus()).isEqualTo(Status.TEAPOT);
+    }
+
+    private HttpContext buildContext() {
+        ClientInfoContext clientInfoContext = ClientInfoContext.builder(new RootContext()).certificates().build();
+        HttpContext context = new HttpContext(clientInfoContext, new SimpleMapSession());
+        return context;
+    }
+
+    private static class SimpleMapSession extends HashMap<String, Object> implements Session {
+        private static final long serialVersionUID = 1L;
+
+        @Override
+        public void save(Response response) throws IOException { }
     }
 }
