5a3b0555463e45bbe41d8ba9fa744a66fd5cefa4
violette <violette.roche-montane@forgerock.com>
2015-10-05 09:25:48 +0200

    OPENIG-435 Part II. Implement OpenAMPolicyEnforcement Filter
    OpenIG uses this new filter to send a request to OpenAM
     to evaluate policy on the current requested uri. The request is
    intercepted by the new SSOTokenFilter, which verifies the validity
    of the token placed in the header iPlanetDirectoryPro if present.
    If is not present or revoked, the SSOTokenFilter generates a new one
     and place it into the request and the process continues. OpenAM sends
    the policy decision and OpenIG interpretes it, allowing the current
     request to the resource or denies it by sending a UNAUTHORIZED response.

---
+++ openig-openam/src/test/java/org/forgerock/openig/openam/PolicyEnforcementFilterTest.java
@@ -0,0 +1,269 @@
+/*
+ * The contents of this file are subject to the terms of the Common Development and
+ * Distribution License (the License). You may not use this file except in compliance with the
+ * License.
+ *
+ * You can obtain a copy of the License at legal/CDDLv1.0.txt. See the License for the
+ * specific language governing permission and limitations under the License.
+ *
+ * When distributing Covered Software, include this CDDL Header Notice in each file and include
+ * the License file at legal/CDDLv1.0.txt. If applicable, add the following below the CDDL
+ * Header, with the fields enclosed by brackets [] replaced by your own identifying
+ * information: "Portions Copyright [year] [name of copyright owner]".
+ *
+ * Copyright 2015 ForgeRock AS.
+ */
+
+package org.forgerock.openig.openam;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.forgerock.http.protocol.Response.newResponsePromise;
+import static org.forgerock.http.protocol.Status.GATEWAY_TIMEOUT;
+import static org.forgerock.http.protocol.Status.INTERNAL_SERVER_ERROR;
+import static org.forgerock.http.protocol.Status.OK;
+import static org.forgerock.http.protocol.Status.UNAUTHORIZED;
+import static org.forgerock.json.JsonValue.array;
+import static org.forgerock.json.JsonValue.field;
+import static org.forgerock.json.JsonValue.json;
+import static org.forgerock.json.JsonValue.object;
+import static org.forgerock.openig.heap.Keys.HTTP_CLIENT_HEAP_KEY;
+import static org.forgerock.openig.heap.Keys.LOGSINK_HEAP_KEY;
+import static org.forgerock.openig.heap.Keys.TEMPORARY_STORAGE_HEAP_KEY;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+import java.io.IOException;
+import java.net.URI;
+import java.util.HashMap;
+
+import org.forgerock.http.Handler;
+import org.forgerock.http.protocol.Request;
+import org.forgerock.http.protocol.Response;
+import org.forgerock.http.session.Session;
+import org.forgerock.http.session.SessionContext;
+import org.forgerock.json.JsonValue;
+import org.forgerock.json.JsonValueException;
+import org.forgerock.openig.el.Expression;
+import org.forgerock.openig.heap.HeapException;
+import org.forgerock.openig.heap.HeapImpl;
+import org.forgerock.openig.heap.Name;
+import org.forgerock.openig.http.Exchange;
+import org.forgerock.openig.http.HttpClient;
+import org.forgerock.openig.io.TemporaryStorage;
+import org.forgerock.openig.log.ConsoleLogSink;
+import org.forgerock.openig.log.Logger;
+import org.forgerock.services.context.RootContext;
+import org.mockito.Mock;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+@SuppressWarnings("javadoc")
+public class PolicyEnforcementFilterTest {
+    private static final String OPENAM_URI = "http://www.example.com:8090/openam/";
+    private static final String RESOURCE_CONTENT = "Access granted!";
+    private static final String TOKEN = "ARrrg...42*";
+    private static final Object POLICY_DECISION = array(object(
+                                                            field("advices", object()),
+                                                            field("ttl", "9223372036854776000"),
+                                                            field("resource", "http://example.com/resource.jpg"),
+                                                            field("actions", object(field("POST", false),
+                                                                                    field("GET", true))),
+                                                            field("attributes", object())));
+
+    private SessionContext sessionContext;
+    private Exchange exchange;
+
+    @Mock
+    private Handler terminalHandler;
+
+    @Mock
+    private Logger logger;
+
+    @BeforeMethod
+    public void setUp() throws Exception {
+        initMocks(this);
+        sessionContext = new SessionContext(new RootContext(), new SimpleMapSession());
+        exchange = new Exchange(sessionContext, null);
+        exchange.getAttributes().put("password", "hifalutin");
+        exchange.getAttributes().put("ssoTokenSubject", TOKEN);
+    }
+
+    @DataProvider
+    private static Object[][] validConfigurations() {
+        return new Object[][] {
+            { json(object(
+                    field("openamUrl", OPENAM_URI),
+                    field("pepUsername", "jackson"),
+                    field("pepPassword", "password"),
+                    field("ssoTokenSubject", "${exchange.attributes.ssoTokenSubject}"),
+                    field("application", "myApplication"))) },
+            { json(object(
+                    field("openamUrl", OPENAM_URI),
+                    field("pepUsername", "jackson"),
+                    field("pepPassword", "password"),
+                    field("jwtSubject", "${exchange.attributes.jwtSubject}"),
+                    field("application", "anotherApplication"))) } };
+    }
+
+    @DataProvider
+    private static Object[][] invalidConfigurations() {
+        return new Object[][] {
+            /* Missing url to OpenAM. */
+            { json(object(
+                    field("pepUsername", "jackson"),
+                    field("pepPassword", "password"),
+                    field("ssoTokenSubject", "${exchange.attributes.ssoTokenSubject}"))) },
+            /* Missing pepUsername. */
+            { json(object(
+                    field("openamUrl", OPENAM_URI),
+                    field("pepPassword", "password"),
+                    field("ssoTokenSubject", "${exchange.attributes.ssoTokenSubject}"))) },
+            /* Missing pepPassword. */
+            { json(object(
+                    field("openamUrl", OPENAM_URI),
+                    field("pepUsername", "jackson"),
+                    field("ssoTokenSubject", "${exchange.attributes.ssoTokenSubject}"))) },
+            /* Missing ssoTokenSubject OR jwtSubject. */
+            { json(object(
+                    field("openamUrl", OPENAM_URI),
+                    field("pepUsername", "jackson"),
+                    field("pepPassword", "password"))) } };
+    }
+
+    @Test(dataProvider = "invalidConfigurations",
+          expectedExceptions = { JsonValueException.class, HeapException.class })
+    public void shouldFailToCreateHeapletWhenRequiredAttributeIsMissing(final JsonValue config) throws Exception {
+        buildPolicyEnforcementFilter(config);
+    }
+
+    @Test(dataProvider = "validConfigurations")
+    public void shouldSucceedToCreateHeaplet(final JsonValue config) throws Exception {
+        final PolicyEnforcementFilter filter = buildPolicyEnforcementFilter(config);
+        assertThat(filter).isNotNull();
+    }
+
+    @Test
+    public void shouldFailToGetAccessToRequestedResource() throws Exception {
+        // Given
+        final Request request = new Request();
+        request.setMethod("GET");
+        request.setUri("http://example.com/user.1/edit");
+
+        final PolicyEnforcementFilter filter = buildPolicyEnforcementFilter();
+
+        sessionContext.getSession().put("SSOToken", TOKEN);
+        when(terminalHandler.handle(any(Exchange.class), any(Request.class)))
+            .thenReturn(newResponsePromise(policyDecisionAsJsonResponse()));
+
+        // When
+        final Response finalResponse = filter.filter(exchange,
+                                                     request,
+                                                     terminalHandler).get();
+        // Then
+        verify(terminalHandler).handle(any(Exchange.class), any(Request.class));
+        assertThat(finalResponse.getStatus()).isEqualTo(UNAUTHORIZED);
+    }
+
+    @Test
+    public void shouldSucceedToGetAccessToRequestedResource() throws Exception {
+        // Given
+        final Request request = new Request();
+        request.setMethod("GET");
+        request.setUri("http://example.com/resource.jpg");
+
+        final PolicyEnforcementFilter filter = buildPolicyEnforcementFilter();
+
+        sessionContext.getSession().put("SSOToken", TOKEN);
+        when(terminalHandler.handle(any(Exchange.class), any(Request.class)))
+            .thenReturn(newResponsePromise(policyDecisionAsJsonResponse()))
+            .thenReturn(newResponsePromise(displayResourceResponse()));
+
+        // When
+        final Response finalResponse = filter.filter(exchange,
+                                                     request,
+                                                     terminalHandler).get();
+        // Then
+        verify(terminalHandler, times(2)).handle(any(Exchange.class), any(Request.class));
+        assertThat(finalResponse.getStatus()).isEqualTo(OK);
+        assertThat(finalResponse.getEntity().getString()).isEqualTo(RESOURCE_CONTENT);
+    }
+
+    @Test
+    public void shouldFailDueToInvalidServerResponse() throws Exception {
+        // Given
+        final Request request = new Request();
+        request.setMethod("GET");
+        request.setUri("http://example.com/resource.jpg");
+
+        final Response errorResponse = new Response();
+        errorResponse.setStatus(GATEWAY_TIMEOUT);
+        errorResponse.setEntity(array(json(field("error", "Something has gone wrong"))));
+
+        final PolicyEnforcementFilter filter = buildPolicyEnforcementFilter();
+        filter.setLogger(logger);
+
+        sessionContext.getSession().put("SSOToken", TOKEN);
+        when(terminalHandler.handle(any(Exchange.class), any(Request.class)))
+            .thenReturn(newResponsePromise(errorResponse));
+
+        // When
+        final Response finalResponse = filter.filter(exchange,
+                                                     request,
+                                                     terminalHandler).get();
+        // Then
+        verify(terminalHandler).handle(any(Exchange.class), any(Request.class));
+        assertThat(finalResponse.getStatus()).isEqualTo(INTERNAL_SERVER_ERROR);
+        assertThat(finalResponse.getEntity().getString()).isEmpty();
+        verify(logger).debug(any(Exception.class));
+    }
+
+    private static Response policyDecisionAsJsonResponse() {
+        final Response response = new Response();
+        response.setStatus(OK);
+        response.setEntity(POLICY_DECISION);
+        return response;
+    }
+
+    private static Response displayResourceResponse() {
+        final Response response = new Response();
+        response.setStatus(OK);
+        response.setEntity(RESOURCE_CONTENT);
+        return response;
+    }
+
+    private PolicyEnforcementFilter buildPolicyEnforcementFilter() throws Exception {
+        final PolicyEnforcementFilter filter = new PolicyEnforcementFilter(URI.create(OPENAM_URI),
+                                                                           null,
+                                                                           terminalHandler);
+        filter.setSsoTokenSubject(Expression.valueOf("${exchange.attributes.ssoTokenSubject}", String.class));
+        return filter;
+    }
+
+    private static PolicyEnforcementFilter buildPolicyEnforcementFilter(final JsonValue config) throws Exception {
+        final PolicyEnforcementFilter.Heaplet heaplet = new PolicyEnforcementFilter.Heaplet();
+        return (PolicyEnforcementFilter) heaplet.create(Name.of("myAssignmentFilter"),
+                                                                config,
+                                                                buildDefaultHeap());
+    }
+
+    private static class SimpleMapSession extends HashMap<String, Object> implements Session {
+        private static final long serialVersionUID = 1L;
+
+        @Override
+        public void save(Response response) throws IOException {
+            // Nothing to do.
+        }
+    }
+
+    public static HeapImpl buildDefaultHeap() throws Exception {
+        final HeapImpl heap = new HeapImpl(Name.of("myHeap"));
+        heap.put(TEMPORARY_STORAGE_HEAP_KEY, new TemporaryStorage());
+        heap.put(LOGSINK_HEAP_KEY, new ConsoleLogSink());
+        heap.put(HTTP_CLIENT_HEAP_KEY, new HttpClient());
+        return heap;
+    }
+}
