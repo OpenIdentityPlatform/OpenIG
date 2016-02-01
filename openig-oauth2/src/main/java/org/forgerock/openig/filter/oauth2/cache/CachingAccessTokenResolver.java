f6e78dc2634433e82c230ff2e088ff9498b601ef
Laurent Vaills <laurent.vaills@forgerock.com>
2016-02-01 13:20:11 +0100

    OPENIG-850 Use the expires_in info when caching the access token

--- openig-oauth2/src/main/java/org/forgerock/openig/filter/oauth2/cache/CachingAccessTokenResolver.java
+++ openig-oauth2/src/main/java/org/forgerock/openig/filter/oauth2/cache/CachingAccessTokenResolver.java
@@ -18,14 +18,18 @@ package org.forgerock.openig.filter.oauth2.cache;
 
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 
 import org.forgerock.openig.filter.oauth2.AccessToken;
 import org.forgerock.openig.filter.oauth2.AccessTokenResolver;
 import org.forgerock.openig.filter.oauth2.OAuth2TokenException;
 import org.forgerock.openig.util.ThreadSafeCache;
 import org.forgerock.services.context.Context;
+import org.forgerock.util.AsyncFunction;
+import org.forgerock.util.Function;
 import org.forgerock.util.promise.Promise;
 import org.forgerock.util.promise.Promises;
+import org.forgerock.util.time.Duration;
 
 /**
  * A {@link CachingAccessTokenResolver} is a delegating {@link AccessTokenResolver} that uses a write-through cache
@@ -59,7 +63,7 @@ public class CachingAccessTokenResolver implements AccessTokenResolver {
                 public Promise<AccessToken, OAuth2TokenException> call() throws Exception {
                     return resolver.resolve(context, token);
                 }
-            });
+            }, expires());
         } catch (InterruptedException e) {
             return Promises.newExceptionPromise(
                     new OAuth2TokenException("Timed out retrieving OAuth2 access token information", e));
@@ -67,5 +71,24 @@ public class CachingAccessTokenResolver implements AccessTokenResolver {
             return Promises.newExceptionPromise(
                     new OAuth2TokenException("Initial token resolution has failed", e));
         }
+    }
+
+    private AsyncFunction<Promise<AccessToken, OAuth2TokenException>, Duration, Exception> expires() {
+        return new AsyncFunction<Promise<AccessToken, OAuth2TokenException>, Duration, Exception>() {
+            @Override
+            public Promise<? extends Duration, ? extends Exception> apply(
+                    Promise<AccessToken, OAuth2TokenException> accessTokenPromise)
+                    throws Exception {
+                return accessTokenPromise.then(new Function<AccessToken, Duration, OAuth2TokenException>() {
+                    @Override
+                    public Duration apply(AccessToken accessToken) throws OAuth2TokenException {
+                        if (accessToken.getExpiresAt() == AccessToken.NEVER_EXPIRES) {
+                            return Duration.duration("unlimited");
+                        }
+                        return new Duration(accessToken.getExpiresAt(), TimeUnit.MILLISECONDS);
+                    }
+                });
+            }
+        };
     }
 }
