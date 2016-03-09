a4e5252acb2d70f6030017a600d5160900fbc30f
violette <violette.roche-montane@forgerock.com>
2016-03-09 15:56:53 +0100

    OPENIG-880 Add ConditionEnforcementFilter to OpenIG toolbox
    ConditionEnforcementFilter will replace the EnforcerFilter provided in the oauth2 folder.
    This filter makes sure that the handled {@link Request} verifies a given condition.
     If the condition is verified, the chain of execution continues.
     If not, it returns a {@literal 403} Forbidden response by default or could refers to a failure handler.

---
+++ openig-core/src/main/java/org/forgerock/openig/filter/ConditionEnforcementFilter.java
@@ -0,0 +1,130 @@
+/*
+ * The contents of this file are subject to the terms of the Common Development and
+ * Distribution License (the License). You may not use this file except in compliance with the
+ * License.
+ *
+ * You can obtain a copy of the License at legal/CDDLv1.0.txt. See the License for the
+ * specific language governing permission and limitations under the License.
+ *
+ * When distributing Covered Software, include this CDDL Header Notice in each file and include
+ * the License file at legal/CDDLv1.0.txt. If applicable, add the following below the CDDL
+ * Header, with the fields enclosed by brackets [] replaced by your own identifying
+ * information: "Portions copyright [year] [name of copyright owner]".
+ *
+ * Copyright 2016 ForgeRock AS.
+ */
+package org.forgerock.openig.filter;
+
+import static java.lang.Boolean.TRUE;
+import static java.lang.String.format;
+import static org.forgerock.openig.el.Bindings.bindings;
+import static org.forgerock.openig.util.JsonValues.asExpression;
+import static org.forgerock.util.Reject.checkNotNull;
+
+import org.forgerock.http.Filter;
+import org.forgerock.http.Handler;
+import org.forgerock.http.protocol.Request;
+import org.forgerock.http.protocol.Response;
+import org.forgerock.openig.el.Bindings;
+import org.forgerock.openig.el.Expression;
+import org.forgerock.openig.handler.Handlers;
+import org.forgerock.openig.heap.GenericHeapObject;
+import org.forgerock.openig.heap.GenericHeaplet;
+import org.forgerock.openig.heap.HeapException;
+import org.forgerock.services.context.Context;
+import org.forgerock.util.promise.NeverThrowsException;
+import org.forgerock.util.promise.Promise;
+
+/**
+ * An {@link ConditionEnforcementFilter} makes sure that the handled {@link Request} verifies
+ * a condition. If the condition is verified, the chain of execution continues.
+ * If the condition is not verified, it returns a {@literal 403} Forbidden response by default,
+ * that actually stops the chain or could refers to a failure handler.
+ * <p>
+ * Configuration options:
+ *
+ * <pre>
+ * {@code
+ * {
+ *     "condition"              : expression,         [REQUIRED]
+ *     "failureHandler          : handler             [OPTIONAL - default to 403]
+ * }
+ * }
+ *
+ * </pre>
+ *
+ * For example:
+ *
+ * <pre>
+ * {@code
+ * {
+ *     "type": "ConditionEnforcementFilter",
+ *     "config": {
+ *         "condition"             : "${not empty(attributes.myAttribute)}",
+ *         "failureHandler"        : "ConditionFailed"
+ *     }
+ * }
+ * }
+ * </pre>
+ */
+public class ConditionEnforcementFilter extends GenericHeapObject implements Filter {
+
+    private final Expression<Boolean> condition;
+    private final Handler failureHandler;
+
+    /**
+     * Creates a new {@link ConditionEnforcementFilter}. If the condition fails,
+     * default {@link Handlers#FORBIDDEN} will be used.
+     *
+     * @param condition
+     *            {@link Expression} that needs to evaluates to {@literal true}
+     *            to continue the chain of execution.
+     */
+    public ConditionEnforcementFilter(final Expression<Boolean> condition) {
+        this(condition, Handlers.FORBIDDEN);
+    }
+
+    /**
+     * Creates a new {@link ConditionEnforcementFilter}.
+     *
+     * @param condition
+     *            {@link Expression} that needs to evaluates to {@literal true}
+     *            to continue the chain of execution.
+     * @param failureHandler
+     *            The handler which will be invoked when condition fails.
+     */
+    public ConditionEnforcementFilter(final Expression<Boolean> condition, final Handler failureHandler) {
+        this.condition = checkNotNull(condition);
+        this.failureHandler = checkNotNull(failureHandler);
+    }
+
+    @Override
+    public Promise<Response, NeverThrowsException> filter(final Context context,
+                                                          final Request request,
+                                                          final Handler next) {
+        if (!isConditionVerified(bindings(context, request))) {
+            logger.debug(format("Cannot satisfy the enforcement's condition expression '%s'", condition.toString()));
+            return failureHandler.handle(context, request);
+        }
+        return next.handle(context, request);
+    }
+
+    private boolean isConditionVerified(final Bindings bindings) {
+        return TRUE.equals(condition.eval(bindings));
+    }
+
+    /** Creates and initializes an ConditionEnforcementFilter in a heap environment. */
+    public static class Heaplet extends GenericHeaplet {
+        @Override
+        public Object create() throws HeapException {
+
+            final Expression<Boolean> condition = asExpression(config.get("condition").required(), Boolean.class);
+            if (config.isDefined("failureHandler")) {
+                return new ConditionEnforcementFilter(condition, heap.resolve(config.get("failureHandler"),
+                                                                              Handler.class));
+            } else {
+                return new ConditionEnforcementFilter(condition);
+            }
+        }
+    }
+}
---
+++ openig-core/src/test/java/org/forgerock/openig/filter/ConditionEnforcementFilterTest.java
@@ -0,0 +1,173 @@
+/*
+ * The contents of this file are subject to the terms of the Common Development and
+ * Distribution License (the License). You may not use this file except in compliance with the
+ * License.
+ *
+ * You can obtain a copy of the License at legal/CDDLv1.0.txt. See the License for the
+ * specific language governing permission and limitations under the License.
+ *
+ * When distributing Covered Software, include this CDDL Header Notice in each file and include
+ * the License file at legal/CDDLv1.0.txt. If applicable, add the following below the CDDL
+ * Header, with the fields enclosed by brackets [] replaced by your own identifying
+ * information: "Portions Copyright [year] [name of copyright owner]".
+ *
+ * Copyright 2016 ForgeRock AS.
+ */
+package org.forgerock.openig.filter;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.forgerock.http.protocol.Status.FORBIDDEN;
+import static org.forgerock.json.JsonValue.field;
+import static org.forgerock.json.JsonValue.json;
+import static org.forgerock.json.JsonValue.object;
+import static org.forgerock.openig.heap.Keys.CLIENT_HANDLER_HEAP_KEY;
+import static org.forgerock.openig.heap.Keys.LOGSINK_HEAP_KEY;
+import static org.forgerock.openig.heap.Keys.TEMPORARY_STORAGE_HEAP_KEY;
+import static org.forgerock.util.Options.defaultOptions;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyZeroInteractions;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+import org.forgerock.http.Handler;
+import org.forgerock.http.handler.HttpClientHandler;
+import org.forgerock.http.protocol.Request;
+import org.forgerock.http.protocol.Response;
+import org.forgerock.json.JsonValue;
+import org.forgerock.json.JsonValueException;
+import org.forgerock.openig.el.Expression;
+import org.forgerock.openig.handler.ClientHandler;
+import org.forgerock.openig.heap.HeapImpl;
+import org.forgerock.openig.heap.Name;
+import org.forgerock.openig.io.TemporaryStorage;
+import org.forgerock.openig.log.ConsoleLogSink;
+import org.forgerock.openig.log.Logger;
+import org.forgerock.services.context.Context;
+import org.forgerock.services.context.RootContext;
+import org.mockito.Mock;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+@SuppressWarnings("javadoc")
+public class ConditionEnforcementFilterTest {
+
+    private Context context;
+
+    @Mock
+    private Handler failureHandler;
+
+    @Mock
+    private Handler next;
+
+    @Mock
+    private Logger logger;
+
+    @BeforeMethod
+    public void setUp() throws Exception {
+        initMocks(this);
+        context = new RootContext();
+    }
+    @DataProvider
+    private static Object[][] validConfigurations() {
+        return new Object[][] {
+            { json(object(
+                    field("condition", "${false}"))) },
+            { json(object(
+                    field("condition", "${true}"),
+                    field("failureHandler", "failureHandler"))) } };
+    }
+
+    @Test(dataProvider = "validConfigurations")
+    public void shouldCreateHeaplet(final JsonValue config) throws Exception {
+        final ConditionEnforcementFilter.Heaplet heaplet = new ConditionEnforcementFilter.Heaplet();
+        final ConditionEnforcementFilter filter = (ConditionEnforcementFilter) heaplet.create(Name.of("myFilter"),
+                                                                                              config,
+                                                                                              buildDefaultHeap());
+        assertThat(filter).isNotNull();
+    }
+
+    @DataProvider
+    private static Object[][] invalidConfigurations() {
+        return new Object[][] {
+            /** Missing condition attribute. */
+            { json(object(
+                    field("conditiontypo", "${false}"))) },
+            /** Invalid condition attribute. */
+            { json(object(
+                    field("condition", 3.141592))) },
+            /** Invalid failure handler. */
+            { json(object(
+                    field("condition", "${true}"),
+                    field("failureHandler", "unknownInHeap"))) },
+            /** Invalid value for the failure handler. */
+            { json(object(
+                    field("condition", "${true}"),
+                    field("failureHandler", 42))) } };
+    }
+
+    @Test(dataProvider = "invalidConfigurations", expectedExceptions = JsonValueException.class)
+    public void shouldNotCreateHeaplet(final JsonValue invalidConfiguration) throws Exception {
+        final ConditionEnforcementFilter.Heaplet heaplet = new ConditionEnforcementFilter.Heaplet();
+        heaplet.create(Name.of("myFilter"), invalidConfiguration, buildDefaultHeap());
+    }
+
+    @Test
+    public void shouldContinueChainExecutionWhenConditionIsTrue() throws Exception {
+        final ConditionEnforcementFilter filter = new ConditionEnforcementFilter(Expression.valueOf("${true}",
+                                                                                                    Boolean.class));
+        filter.filter(context, null, next);
+        verify(next).handle(any(Context.class), any(Request.class));
+        verifyZeroInteractions(logger);
+    }
+
+    @DataProvider
+    public static Object[][] conditionsEvaluatingToFalse() {
+        return new Object[][] {
+            { "${false}" },
+            { "not a condition" },
+            { "${attributes.missing}" }
+        };
+    }
+
+    @Test(dataProvider = "conditionsEvaluatingToFalse")
+    public void shouldReturnForbiddenAndBreakChainExecutionWhenConditionIsFalse(final String condition)
+            throws Exception {
+        // Given
+        final ConditionEnforcementFilter filter = new ConditionEnforcementFilter(Expression.valueOf(condition,
+                                                                                                    Boolean.class));
+        filter.setLogger(logger);
+        // When
+        final Response response = filter.filter(context, null, next).get();
+        // Then
+        assertThat(response.getStatus()).isEqualTo(FORBIDDEN);
+        assertThat(response.getCause()).isNull();
+        verify(logger).debug(anyString());
+        verifyZeroInteractions(next);
+    }
+
+    @Test(dataProvider = "conditionsEvaluatingToFalse")
+    public void shouldDelegateToFailureHandlerForInvalidOrEvaluatedToFalseConditions(final String condition)
+            throws Exception {
+        // Given
+        final ConditionEnforcementFilter filter = new ConditionEnforcementFilter(Expression.valueOf(condition,
+                                                                                                    Boolean.class),
+                                                                                 failureHandler);
+        filter.setLogger(logger);
+        // When
+        filter.filter(context, null, next);
+        // Then
+        verify(failureHandler).handle(any(Context.class), any(Request.class));
+        verify(logger).debug(anyString());
+    }
+
+    private HeapImpl buildDefaultHeap() throws Exception {
+        final HeapImpl heap = new HeapImpl(Name.of("myHeap"));
+        heap.put(TEMPORARY_STORAGE_HEAP_KEY, new TemporaryStorage());
+        heap.put(LOGSINK_HEAP_KEY, new ConsoleLogSink());
+        heap.put(CLIENT_HANDLER_HEAP_KEY, new ClientHandler(new HttpClientHandler(defaultOptions())));
+        heap.put("failureHandler", failureHandler);
+        return heap;
+    }
+}
