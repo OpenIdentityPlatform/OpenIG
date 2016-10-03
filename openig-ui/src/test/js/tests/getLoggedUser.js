b90d677260571a3824c1113b32b6f97694a80bc9
Ludek Hon <ludek.hon@profiq.com>
2016-10-03 15:09:06 +0200

    Fix licenses

--- openig-ui/src/test/js/tests/getLoggedUser.js
+++ openig-ui/src/test/js/tests/getLoggedUser.js
@@ -11,7 +11,7 @@
  * Header, with the fields enclosed by brackets [] replaced by your own identifying
  * information: "Portions copyright [year] [name of copyright owner]".
  *
- * Copyright 2014-2016 ForgeRock AS.
+ * Copyright 2016 ForgeRock AS.
  */
 
 define([
