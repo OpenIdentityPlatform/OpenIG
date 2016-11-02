74c94e601dc092f0f5e45155aa51335a701e62e7
Ludek Hon <ludek.hon@profiq.com>
2016-11-02 16:28:28 +0100

    OPENIG-1391 Add footer info

---
+++ openig-ui/src/main/js/org/forgerock/openig/ui/admin/services/ServerInfoService.js
@@ -0,0 +1,49 @@
+/**
+ * The contents of this file are subject to the terms of the Common Development and
+ * Distribution License (the License). You may not use this file except in compliance with the
+ * License.
+ *
+ * You can obtain a copy of the License at legal/CDDLv1.0.txt. See the License for the
+ * specific language governing permission and limitations under the License.
+ *
+ * When distributing Covered Software, include this CDDL Header Notice in each file and include
+ * the License file at legal/CDDLv1.0.txt. If applicable, add the following below the CDDL
+ * Header, with the fields enclosed by brackets [] replaced by your own identifying
+ * information: "Portions copyright [year] [name of copyright owner]".
+ *
+ * Copyright 2016 ForgeRock AS.
+ */
+
+define([
+    "jquery",
+    "org/forgerock/commons/ui/common/util/Constants",
+    "org/forgerock/commons/ui/common/main/AbstractDelegate"
+], (
+    $,
+    Constants,
+    AbstractDelegate
+) => {
+    class ServerInfoService {
+        constructor () {
+            this.infoDelegate = new AbstractDelegate(`${Constants.apiPath}/info`);
+        }
+
+        getInfo () {
+            const deferred = $.Deferred();
+            if (this.dataCache) {
+                return deferred.resolve(this.dataCache);
+            }
+            this.infoDelegate.serviceCall({})
+                .done((data) => {
+                    this.dataCache = data;
+                    deferred.resolve(data);
+                })
+                .fail(() => {
+                    console.log("ServerInfoService - no info data");
+                    deferred.resolve();
+                });
+            return deferred;
+        }
+    }
+    return new ServerInfoService();
+});
