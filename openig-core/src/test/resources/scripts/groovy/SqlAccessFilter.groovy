6236a2466edd49bdad4141f831d476f965007646
Guillaume Sauthier <guillaume.sauthier@forgerock.com>
2016-11-10 22:07:38 +0100

    Add forgotten license headers

--- openig-core/src/test/resources/scripts/groovy/SqlAccessFilter.groovy
+++ openig-core/src/test/resources/scripts/groovy/SqlAccessFilter.groovy
@@ -1,4 +1,20 @@
 /*
+ * The contents of this file are subject to the terms of the Common Development and
+ * Distribution License (the License). You may not use this file except in compliance with the
+ * License.
+ *
+ * You can obtain a copy of the License at legal/CDDLv1.0.txt. See the License for the
+ * specific language governing permission and limitations under the License.
+ *
+ * When distributing Covered Software, include this CDDL Header Notice in each file and include
+ * the License file at legal/CDDLv1.0.txt. If applicable, add the following below the CDDL
+ * Header, with the fields enclosed by brackets [] replaced by your own identifying
+ * information: "Portions Copyright [year] [name of copyright owner]".
+ *
+ * Copyright 2014-2016 ForgeRock AS.
+ */
+
+/*
  * Look up user credentials in a relational database
  * based on the user's email address provided in the request form data,
  * and set the credentials in the request headers for the next handler.
