b63ca30a549018bf14f878df83554bf38920df86
Roman Schneeweiss <roman.schneeweiss@profiq.com>
2016-11-25 15:13:33 +0100

    OPENIG-1431 Convert Overview into ES6 class

--- openig-ui/src/main/js/org/forgerock/openig/ui/admin/routes/parts/Overview.js
+++ openig-ui/src/main/js/org/forgerock/openig/ui/admin/routes/parts/Overview.js
@@ -27,152 +27,157 @@ define([
     i18n,
     AbstractRouteView
 ) => (
-    AbstractRouteView.extend({
-        element: ".main",
-        template: "templates/openig/admin/routes/parts/Overview.html",
-        partials: [
-            "templates/openig/admin/routes/components/OverviewItem.html"
-        ],
-        initialize (options) {
-            this.data = options.parentData;
-            this.data.routeId = this.data.routeData.get("id");
-            this.data.title = this.data.routeData.get("name");
-            this.data.baseURI = this.data.routeData.get("baseURI");
-            this.data.condition = this.data.routeData.get("condition");
-            this.data.overviewItems = [
-                {
-                    title: i18n.t("config.AppConfiguration.Navigation.routeSideMenu.capture"),
-                    route: "routeCapture",
-                    icon: "fa-search"
-                },
-                {
-                    title: i18n.t("config.AppConfiguration.Navigation.routeSideMenu.throttling"),
-                    route: "routeThrottling",
-                    icon: "fa-filter"
-                },
-                {
-                    title: i18n.t("config.AppConfiguration.Navigation.routeSideMenu.authentication"),
-                    route: "routeAuthentication",
-                    icon: "fa-user"
-                },
-                {
-                    title: i18n.t("config.AppConfiguration.Navigation.routeSideMenu.authorization"),
-                    route: "routeAuthorization",
-                    icon: "fa-key"
-                },
-                {
-                    title: i18n.t("config.AppConfiguration.Navigation.routeSideMenu.statistics"),
-                    route: "routeStatistics",
-                    icon: "fa-line-chart"
-                }
-            ];
-        },
-        data: {
-        },
-        render () {
-            _.forEach(this.data.overviewItems, (item) => {
-                item.routeId = this.data.routeId;
-                item.status = this.getStatus(item.route);
-            });
-
-            this.parentRender();
-        },
-        getStatus (route) {
-            const filters = this.data.routeData.get("filters");
-            let status = i18n.t("templates.routes.filters.Off");
-            let filter;
-            switch (route) {
-                case "routeCapture":
-                    const captureStatus = this.getCaptureStatus();
-                    if (captureStatus !== "") {
-                        status = captureStatus;
+        class Overview extends AbstractRouteView {
+            constructor (options) {
+                super();
+                this.element = ".main";
+                this.data = options.parentData;
+                this.data.routeId = this.data.routeData.get("id");
+                this.data.title = this.data.routeData.get("name");
+                this.data.baseURI = this.data.routeData.get("baseURI");
+                this.data.condition = this.data.routeData.get("condition");
+                this.data.overviewItems = [
+                    {
+                        title: i18n.t("config.AppConfiguration.Navigation.routeSideMenu.capture"),
+                        route: "routeCapture",
+                        icon: "fa-search"
+                    },
+                    {
+                        title: i18n.t("config.AppConfiguration.Navigation.routeSideMenu.throttling"),
+                        route: "routeThrottling",
+                        icon: "fa-filter"
+                    },
+                    {
+                        title: i18n.t("config.AppConfiguration.Navigation.routeSideMenu.authentication"),
+                        route: "routeAuthentication",
+                        icon: "fa-user"
+                    },
+                    {
+                        title: i18n.t("config.AppConfiguration.Navigation.routeSideMenu.authorization"),
+                        route: "routeAuthorization",
+                        icon: "fa-key"
+                    },
+                    {
+                        title: i18n.t("config.AppConfiguration.Navigation.routeSideMenu.statistics"),
+                        route: "routeStatistics",
+                        icon: "fa-line-chart"
                     }
-                    break;
-                case "routeThrottling":
-                    filter = _.find(filters, {
-                        "type": "ThrottlingFilter",
-                        "enabled": true
-                    });
-                    if (filter) {
-                        status = i18n.t("templates.routes.filters.ThrottlingFilter", {
-                            numberOfRequests: filter.numberOfRequests,
-                            duration: filter.durationValue,
-                            durationRange: i18n.t(`common.timeSlot.${filter.durationRange}`)
+                ];
+            }
+
+            get template () {
+                return "templates/openig/admin/routes/parts/Overview.html";
+            }
+
+            get partials () {
+                return ["templates/openig/admin/routes/components/OverviewItem.html"];
+            }
+
+            render () {
+                _.forEach(this.data.overviewItems, (item) => {
+                    item.routeId = this.data.routeId;
+                    item.status = this.getStatus(item.route);
+                });
+
+                this.parentRender();
+            }
+            getStatus (route) {
+                const filters = this.data.routeData.get("filters");
+                let status = i18n.t("templates.routes.filters.Off");
+                let filter;
+                switch (route) {
+                    case "routeCapture":
+                        const captureStatus = this.getCaptureStatus();
+                        if (captureStatus !== "") {
+                            status = captureStatus;
+                        }
+                        break;
+                    case "routeThrottling":
+                        filter = _.find(filters, {
+                            "type": "ThrottlingFilter",
+                            "enabled": true
                         });
-                    }
-                    break;
-                case "routeAuthentication":
-                    filter = _.find(filters, {
-                        "type": "OAuth2ClientFilter",
-                        "enabled": true
-                    });
-                    if (filter) {
-                        status = i18n.t("templates.routes.filters.OAuth2ClientFilter");
-                    }
-                    filter = _.find(filters, {
-                        "type": "SingleSignOnFilter",
-                        "enabled": true
-                    });
-                    if (filter) {
-                        status = i18n.t("templates.routes.filters.SingleSignOnFilter");
-                    }
-                    break;
-                case "routeAuthorization":
-                    filter = _.find(filters, {
-                        "type": "PolicyEnforcementFilter",
-                        "enabled": true
-                    });
-                    if (filter) {
-                        status = i18n.t("templates.routes.filters.PolicyEnforcementFilter");
-                    }
-                    break;
-                case "routeStatistics":
-                    const statistics = this.data.routeData.get("statistics");
-                    if (_.get(statistics, "enabled")) {
-                        status = i18n.t("templates.routes.parts.statistics.fields.status");
-                    }
-            }
-            return status;
-        },
-
-        getCaptureStatus () {
-            const capture = this.data.routeData.get("capture");
-            let entity;
-            if (_.get(capture, "entity")) {
-                entity = i18n.t("templates.routes.capture.entity");
-            }
-
-            let inbound;
-            if (_.get(capture, "inbound.request") && _.get(capture, "inbound.response")) {
-                inbound = i18n.t("templates.routes.capture.inboundMessages");
-            } else if (_.get(capture, "inbound.request")) {
-                inbound = i18n.t("templates.routes.capture.inboundRequests");
-            } else if (_.get(capture, "inbound.response")) {
-                inbound = i18n.t("templates.routes.capture.inboundResponses");
-            }
-
-            let outbound;
-            if (_.get(capture, "outbound.request") && _.get(capture, "outbound.response")) {
-                outbound = i18n.t("templates.routes.capture.outboundMessages");
-            } else if (_.get(capture, "outbound.request")) {
-                outbound = i18n.t("templates.routes.capture.outboundRequests");
-            } else if (_.get(capture, "outbound.response")) {
-                outbound = i18n.t("templates.routes.capture.outboundResponses");
-            }
-
-            const messageArr = [entity, inbound, outbound];
-            let messages = "";
-            _.forEach(messageArr, (value) => {
-                if (value) {
-                    if (messages === "") {
-                        messages += value;
-                    } else {
-                        messages += `,  ${value}`;
-                    }
+                        if (filter) {
+                            status = i18n.t("templates.routes.filters.ThrottlingFilter", {
+                                numberOfRequests: filter.numberOfRequests,
+                                duration: filter.durationValue,
+                                durationRange: i18n.t(`common.timeSlot.${filter.durationRange}`)
+                            });
+                        }
+                        break;
+                    case "routeAuthentication":
+                        filter = _.find(filters, {
+                            "type": "OAuth2ClientFilter",
+                            "enabled": true
+                        });
+                        if (filter) {
+                            status = i18n.t("templates.routes.filters.OAuth2ClientFilter");
+                        }
+                        filter = _.find(filters, {
+                            "type": "SingleSignOnFilter",
+                            "enabled": true
+                        });
+                        if (filter) {
+                            status = i18n.t("templates.routes.filters.SingleSignOnFilter");
+                        }
+                        break;
+                    case "routeAuthorization":
+                        filter = _.find(filters, {
+                            "type": "PolicyEnforcementFilter",
+                            "enabled": true
+                        });
+                        if (filter) {
+                            status = i18n.t("templates.routes.filters.PolicyEnforcementFilter");
+                        }
+                        break;
+                    case "routeStatistics":
+                        const statistics = this.data.routeData.get("statistics");
+                        if (_.get(statistics, "enabled")) {
+                            status = i18n.t("templates.routes.parts.statistics.fields.status");
+                        }
                 }
-            });
+                return status;
+            }
 
-            return messages;
+            getCaptureStatus () {
+                const capture = this.data.routeData.get("capture");
+                let entity;
+                if (_.get(capture, "entity")) {
+                    entity = i18n.t("templates.routes.capture.entity");
+                }
+
+                let inbound;
+                if (_.get(capture, "inbound.request") && _.get(capture, "inbound.response")) {
+                    inbound = i18n.t("templates.routes.capture.inboundMessages");
+                } else if (_.get(capture, "inbound.request")) {
+                    inbound = i18n.t("templates.routes.capture.inboundRequests");
+                } else if (_.get(capture, "inbound.response")) {
+                    inbound = i18n.t("templates.routes.capture.inboundResponses");
+                }
+
+                let outbound;
+                if (_.get(capture, "outbound.request") && _.get(capture, "outbound.response")) {
+                    outbound = i18n.t("templates.routes.capture.outboundMessages");
+                } else if (_.get(capture, "outbound.request")) {
+                    outbound = i18n.t("templates.routes.capture.outboundRequests");
+                } else if (_.get(capture, "outbound.response")) {
+                    outbound = i18n.t("templates.routes.capture.outboundResponses");
+                }
+
+                const messageArr = [entity, inbound, outbound];
+                let messages = "";
+                _.forEach(messageArr, (value) => {
+                    if (value) {
+                        if (messages === "") {
+                            messages += value;
+                        } else {
+                            messages += `,  ${value}`;
+                        }
+                    }
+                });
+
+                return messages;
+            }
         }
-    })
-));
+    )
+);
