d697e6e0484d22bfef81173ba21293424bacae9b
maximthomas <maxim.thomas@gmail.com>
2025-01-14 08:55:50 +0300

    migrate to JDK 11 and javax -> jakarta initial commit

--- openig-saml/src/main/java/org/forgerock/openig/handler/saml/SamlFederationHandler.java
+++ openig-saml/src/main/java/org/forgerock/openig/handler/saml/SamlFederationHandler.java
@@ -12,6 +12,7 @@
  * information: "Portions Copyright [year] [name of copyright owner]".
  *
  * Copyright 2014-2016 ForgeRock AS.
+ * Portions copyright 2025 3A Systems LLC.
  */
 package org.forgerock.openig.handler.saml;
 
@@ -28,9 +29,9 @@ import java.util.Map;
 import java.util.Properties;
 import java.util.Set;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 import org.forgerock.http.Handler;
 import org.forgerock.http.header.LocationHeader;
