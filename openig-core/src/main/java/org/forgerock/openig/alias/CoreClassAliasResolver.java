abf59613f844facc9c89fd60d2e10c99c6f10625
maximthomas <maxim.thomas@gmail.com>
2026-02-09 11:27:01 +0300

    update copyright

--- openig-core/src/main/java/org/forgerock/openig/alias/CoreClassAliasResolver.java
+++ openig-core/src/main/java/org/forgerock/openig/alias/CoreClassAliasResolver.java
@@ -12,6 +12,7 @@
  * information: "Portions copyright [year] [name of copyright owner]".
  *
  * Copyright 2014-2016 ForgeRock AS.
+ * Portions Copyright 2021-2026 3A Systems LLC.
  */
 
 package org.forgerock.openig.alias;
